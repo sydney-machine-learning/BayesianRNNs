8905e0c34398a999931f089bdc348cfbc40ef1a0
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2021-10-11 13:32:27 +0530

    meeting_oct_11_monday

--- Multistep/time_series_mcmc.py
+++ Multistep/time_series_mcmc.py
@@ -16,20 +16,20 @@ import gc
 
 mpl.use('agg')
 parser=argparse.ArgumentParser(description='PTBayeslands modelling')
-parser.add_argument('-s','--samples', help='Number of samples', default=100, dest="samples",type=int)
+parser.add_argument('-s','--samples', help='Number of samples', default=4000, dest="samples",type=int)
 # parser.add_argument('-r','--replicas', help='Number of chains/replicas, best to have one per availble core/cpu', default=8,dest="num_chains",type=int)
 # parser.add_argument('-t','--temperature', help='Demoninator to determine Max Temperature of chains (MT=no.chains*t) ', default=2,dest="mt_val",type=int)
 # parser.add_argument('-swap','--swap', help='Swap Ratio', dest="swap_ratio",default=0.001,type=float)
-parser.add_argument('-b','--burn', help='How many samples to discard before determing posteriors', dest="burn_in",default = 0.6,type=float)
+parser.add_argument('-b','--burn', help='How many samples to discard before determing posteriors', dest="burn_in",default = 0.5,type=float)
 # parser.add_argument('-pt','--ptsamples', help='Ratio of PT vs straight MCMC samples to run', dest="pt_samples",default=0.5,type=float)
 parser.add_argument('-step','--step', help='Step size for proposals (0.02, 0.05, 0.1 etc)', dest="step_size",default=0.025,type=float)
 parser.add_argument('-lr','--learn', help='learn rate for langevin gradient', dest="learn_rate",default=0.05,type=float)
 parser.add_argument('-m','--model', help='1 to select RNN, 2 to select LSTM', dest = "net", default = 2, type= int)
-parser.add_argument('-o','--optim', help='1 to select SGD, 2 to select Adam', dest = 'optimizer', default = 2, type = int)
+parser.add_argument('-o','--optim', help='1 to select SGD, 2 to select Adam', dest = 'optimizer', default = 1, type = int)
 parser.add_argument('-debug','--debug', help='debug = 0 or 1; when 1 trace plots will not be produced', dest= 'DEBUG', default= 1, type= bool)
 args = parser.parse_args()
 
-
+#tinker step_w to 0.05.....0.01;
 
 class Model(nn.Module):
 		# Defining input size, hidden layer size, output size and batch size respectively
@@ -95,7 +95,7 @@ class Model(nn.Module):
 		self.load_state_dict(w)
 
 		criterion = torch.nn.MSELoss()
-		for k in range(10):
+		for k in range(1):
 			output = self.forward(x)
 			optimizer.zero_grad()
 			loss = criterion(output, y.view(output.shape))
@@ -103,7 +103,7 @@ class Model(nn.Module):
 			loss.backward()
 			optimizer.step()
 
-			print(f'root of MSEloss for k = {k}: {np.sqrt(loss.detach().numpy())}')
+			# print(f'root of MSEloss for k = {k}: {np.sqrt(loss.detach().numpy())}')
 
 		parameters = self.state_dict()
 		return parameters 
@@ -215,7 +215,7 @@ class MCMC:
 		tau_pro = np.exp(eta)
 
 		eta = 0
-		step_w = 0.025
+		step_w = 0.01
 		step_eta = 0.2
 		sigma_squared = 25
 		nu_1 = 0
@@ -227,6 +227,7 @@ class MCMC:
 		[likelihood, pred_train, rmsetrain, step_wise_rmsetrain] = self.likelihood_func(self.rnn, self.train_x, self.train_y, copy.deepcopy(w), tau_pro, temp=1)
 		[_, pred_test, rmsetest, step_wise_rmsetest] = self.likelihood_func(self.rnn, self.test_x, self.test_y, copy.deepcopy(w), tau_pro, temp=1)
 		
+		scaling_factor = 0.01
 		
 		for i in range(self.samples - 1):
 			timer_start = time.time()
@@ -237,10 +238,11 @@ class MCMC:
 				w_prop_gd = copy.deepcopy(self.rnn.langevin_gradient(self.train_x, self.train_y, copy.deepcopy(w_proposal), optimizer= optimizer))
 				wc_delta = self.rnn.getparameters(w) - self.rnn.getparameters(w_prop_gd)
 				wp_delta = self.rnn.getparameters(w_proposal) - self.rnn.getparameters(w_gd)
-				sigma_sq = step_w**2
+				sigma_sq = step_w*step_w
+				# sigma_sq = step_w
 				first = -0.5 * np.sum(wc_delta  *  wc_delta  ) / sigma_sq  # this is wc_delta.T  *  wc_delta /sigma_sq
 				second = -0.5 * np.sum(wp_delta * wp_delta ) / sigma_sq
-				diff_prop =  first - second
+				diff_prop =  (first - second)*scaling_factor
 				langevin_count = langevin_count + 1
 			else:
 				diff_prop = 0
@@ -265,7 +267,7 @@ class MCMC:
 			u = np.log(random.uniform(0,1))
 			prop_list[i+1,] = self.rnn.getparameters(w_proposal).reshape(-1)
 			likeh_list[i+1,0] = likelihood_proposal
-			if u > mh_prob:
+			if u < mh_prob:
 				num_accepted = num_accepted + 1
 				likelihood = likelihood_proposal
 				prior_current = prior_prop 
@@ -286,7 +288,7 @@ class MCMC:
 				step_wise_rmse_test[i+1,] = step_wise_rmse_test[i,]
 
 
-		return(pos_w, rmse_train, step_wise_rmse_train, rmse_test, step_wise_rmse_test, accept_ratio, likeh_list)
+		return(pos_w, rmse_train, step_wise_rmse_train, rmse_test, step_wise_rmse_test, likeh_list)
 		
 def main():
 	n_steps_in, n_steps_out = 5,10
@@ -296,7 +298,7 @@ def main():
 	print("Name of folder to look for: ",os.getcwd()+'/Res_LG-Lprob_'+net+f'_{optimizer}_single_chain/')
 
 	
-	for j in [1, 2, 3] :
+	for j in [5, 3] :
 	# for j in [2]:
 		print(j, ' out of 15','\n\n\n')
 		#i = j//2
@@ -356,8 +358,6 @@ def main():
 		test_y = test_y.reshape(test_y.shape[0],test_y.shape[1],1)
 		print("shapes of train x and y",train_x.shape,train_y.shape)
 
-		a = torch.FloatTensor(train_x)
-		print(a.numpy())
 
 		Hidden = 10 #originally it was 5; but in the paper to compare it is 10
 		topology = [n_steps_in, Hidden,n_steps_out]
@@ -379,7 +379,7 @@ def main():
 					learn_rate = learn_rate, samples= NumSample,trainx= train_x, 
 					trainy= train_y, testx= test_x, testy= test_y,
 					topology= topology,path = path, rnn_net = net, optimizer= optimizer)
-		[pos_w, rmse_train, indiv_rmse_train, rmse_test, indiv_rmse_test ,accept_vec, accept_ratio, likelihood] = mcmc.sampler()
+		[pos_w, rmse_train, indiv_rmse_train, rmse_test, indiv_rmse_test , likelihood] = mcmc.sampler()
 
 
 if __name__ == "__main__":main()
